578f1757b9ab5738e1ac72d52ff790ddf35bdfe0
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-05-22 17:59:09 +0530

    added Generics

---
+++ src/collections/GenericsDemo.java
@@ -0,0 +1,25 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+        List<String> members = new ArrayList<>();
+
+        Consumer<String> consumer = System.out::println;
+
+        members.add("Jackey");
+        members.add("Shivam");
+        members.add("Geeta");
+        members.add("Shradha");
+        members.add("Sita");
+
+//        members.add(10) -> not allowed because list types of string allow only string data types.
+
+        members.forEach(consumer);
+
+    }
+}
