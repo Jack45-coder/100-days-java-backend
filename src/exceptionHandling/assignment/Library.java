2948f5aac91d31e66b6f561c017c40915b70ce53
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-05-24 00:20:07 +0530

    assignment

---
+++ src/exceptionHandling/assignment/Library.java
@@ -0,0 +1,16 @@
+package exceptionHandling.assignment;
+
+public class Library {
+    int availableBooks = 3;
+
+    public void borrowBook(int booksRequested){
+        try {
+            int[] books = {101, 102, 103};
+            System.out.println("Book Requested: " + books[booksRequested]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception Occurred, you requested book that does not exist");
+        }finally {
+            System.out.println("Finally Block");
+        }
+    }
+}
