6923134405a1f60bfb2974d804afc59eb731e9c5
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-05-27 00:23:47 +0530

    functional interface: add MethodReference using forEach and Consumer

---
+++ src/functionalInterface/inbuilt/MethodReferenceDemo.java
@@ -0,0 +1,54 @@
+package functionalInterface.inbuilt;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MethodReferenceDemo {
+    public static void main(String[] args) {
+
+        // ::
+
+        // Sample list of names
+        List<String> names = Arrays.asList("Simple", "Shivani", "Shashi");
+
+        // Traditional for loop
+        for(int i = 0; i < names.size(); i++){
+            System.out.println(names.get(i));
+        }
+
+        System.out.println();
+
+        // Using Enhanced ForEach
+        for (String name : names){
+            System.out.println(name);
+        }
+
+        System.out.println();
+
+        /*
+         // Using Consumer
+        names.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+         */
+/*
+        names.forEach((String name) -> {
+            System.out.println(name);
+        });
+
+ */
+        // Lambda expression with forEach()
+        names.forEach((name) -> System.out.println(name));
+
+        System.out.println();
+
+        // METHOD REFERENCE (::) - shorter form of lambda
+        names.forEach(System.out::println);
+
+    }
+}
