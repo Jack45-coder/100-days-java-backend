f2ecf65cd6ee5a354268c7259c77099d4410659d
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-05-26 18:43:19 +0530

    updated

--- src/functionalInterface/assignment/Main.java
+++ src/functionalInterface/assignment/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Operation operation = (a, b) -> a+b;
 
+        Operation operation1 = (a, b) -> {
+            return a+b;
+        };
+
         System.out.println("Sum: " + operation.add(90, 20));
+        System.out.println(operation1.add(50, 80));
     }
 }
