0055d502eaccd6ba1070021b45b7a40861bd5776
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-05-28 01:14:29 +0530

    Add BiFunction real-world example with map iteration

---
+++ src/functionalInterface/inbuilt/RealExample.java
@@ -0,0 +1,30 @@
+package functionalInterface.inbuilt;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class RealExample {
+    public static void main(String[] args) {
+
+        // <title> costs $<Price>
+
+        BiFunction<String, Double, String> formatter = (title, price) -> title + " costs $" + price;
+
+        Map<String, Double> books = new HashMap<>();
+        books.put("Java Basics", 599.49);
+        books.put("Python", 1599.99);
+        books.put("React", 899.79);
+        books.put("DBMS", 499.49);
+        books.put("AIML", 2999.90);
+        books.put("Electrical Engineering", 1299.90);
+        books.put("HTML", 299.49);
+        books.put("SQL", 199.49);
+
+        for(Map.Entry<String, Double> entry : books.entrySet()){
+            String res = formatter.apply(entry.getKey(), entry.getValue());
+            System.out.println(res);
+        }
+
+    }
+}
