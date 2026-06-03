e41776a91bbbec5296a7d466658f75603e4af4f7
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-06-04 00:27:51 +0530

    Demonstrate error handling in Java streams

---
+++ src/functionalInterface/streams/ErrorHandlingStreams.java
@@ -0,0 +1,23 @@
+package functionalInterface.streams;
+
+import java.util.List;
+
+public class ErrorHandlingStreams {
+    public static void main(String[] args) {
+        List<String> books = List.of("Java", "Python", "C++");
+
+        books.stream()
+                .forEach(b -> {
+                    try {
+                        if (b.equals("C++"))
+                            throw new RuntimeException("Invalid Book");
+                        System.out.println(b);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+        );
+    }
+
+
+}
