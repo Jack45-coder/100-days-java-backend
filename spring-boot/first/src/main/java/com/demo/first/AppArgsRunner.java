6b688fd40589c5f4c7d4fe0cf45858386dd116a1
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-06-30 21:57:22 +0530

    Starter web Dependency Injection advantages

---
+++ spring-boot/first/src/main/java/com/demo/first/AppArgsRunner.java
@@ -0,0 +1,11 @@
+package com.demo.first;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class AppArgsRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+        System.out.println("Options: " +args.getOptionNames());
+    }
+}
