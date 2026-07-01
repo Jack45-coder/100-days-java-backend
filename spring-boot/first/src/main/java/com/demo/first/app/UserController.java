17473bb90891c903350187ddfc7a1b15823f66e5
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-02 01:35:35 +0530

    ResponseEntity implements in UserController

--- spring-boot/first/src/main/java/com/demo/first/app/UserController.java
+++ spring-boot/first/src/main/java/com/demo/first/app/UserController.java
@@ -1,6 +1,8 @@
 package com.demo.first.app;
 
 import com.demo.first.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,23 +16,27 @@ public class UserController {
     private Map<Integer, User> userDb = new HashMap<>();
 
     @PostMapping
-    public String createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         userDb.putIfAbsent(user.getId(), user);
-        return "User Created";
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .body(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @PutMapping
-    public String updateUser(@RequestBody User user) {
-        if (userDb.containsKey(user.getId())) {
-            userDb.put(user.getId(), user);
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        if (!userDb.containsKey(user.getId())) {
+           return ResponseEntity.notFound().build();
         }
-        return "User Updated Successfully ";
+        userDb.put(user.getId(), user);
+        return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable String id){
+    public ResponseEntity<String> deleteUser(@PathVariable int id){
+        if(!userDb.containsKey(id)) return ResponseEntity.notFound().build() ;
         userDb.remove(id);
-        return "User deleted Successfully";
+        return ResponseEntity.ok("User Deleted");
     }
 
     @GetMapping
