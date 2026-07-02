454a807075a4c392eaa43cfff10a65c847741c94
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-03 01:40:12 +0530

    Handle HttpRequestMethodNotSupportedException

--- spring-boot/first/src/main/java/com/demo/first/app/exception/GlobalExceptionHandler.java
+++ spring-boot/first/src/main/java/com/demo/first/app/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.demo.first.app.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +23,17 @@ public class GlobalExceptionHandler {
         errorResponse.put("error", "Bad request");
         errorResponse.put("message", exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodNotSupportedException(
+            Exception exception
+    ){
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status", HttpStatus.METHOD_NOT_ALLOWED.value());
+        errorResponse.put("error", "Method not allowed on this endpoint");
+        errorResponse.put("message", exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
