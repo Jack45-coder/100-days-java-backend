0228521a19aa924666fd6ebf9da25267d01a356b
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-02 15:18:45 +0530

    requestParam with queryParameter

--- spring-boot/first/src/main/java/com/demo/first/app/UserController.java
+++ spring-boot/first/src/main/java/com/demo/first/app/UserController.java
@@ -44,4 +44,44 @@ public class UserController {
     public List<User> getUsers(){
         return new ArrayList<>(userDb.values());
     }
+
+//    @GetMapping({"/users", "/user/{id}"})
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable(value = "userId", required = false) int id){
+        if (!userDb.containsKey(id)){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userDb.get(id));
+    }
+
+    // demo for multiple PathVariable
+    @GetMapping("/{userId}/orders/{orderId}")
+    public ResponseEntity<User> getUserOrder(
+            @PathVariable("userId") int id,
+            @PathVariable int orderId
+    ){
+        System.out.println("ORDER ID: " +orderId);
+        if (!userDb.containsKey(id)){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userDb.get(id));
+    }
+
+
+    // /search?name=john
+    @GetMapping("/search")
+    public ResponseEntity<List<User>> searchUser(
+            @RequestParam(required = false, defaultValue = "Alice") String name,
+            @RequestParam(required = false, defaultValue = "alice@gmail.com") String email
+    ){
+        System.out.println(name);
+
+
+        List<User> users = userDb.values().stream()
+                .filter(user -> name == null || user.getName().equalsIgnoreCase(name))
+                .filter(user -> email == null || user.getEmail().equalsIgnoreCase(email))
+                .toList();
+        return ResponseEntity.ok(users);
+    }
 }
