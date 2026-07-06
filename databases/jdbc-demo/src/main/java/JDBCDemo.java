0a37349657d59ed841b63062ff01a3ddf5079721
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-07 00:33:07 +0530

    Implement JDBC CRUD operations using Statement

--- databases/jdbc-demo/src/main/java/JDBCDemo.java
+++ databases/jdbc-demo/src/main/java/JDBCDemo.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBCDemo {
     private static final String URL = "jdbc:mysql://localhost:3306/demo_db";
@@ -8,6 +6,67 @@ public class JDBCDemo {
     private static final String PASSWORD = "Jack@1234";
 
     public static void main(String[] args) {
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);){
+            System.out.println("Connected to the database");
+//            insertStudent(conn,"Alice", "alice@gmail.com");
+            updateStudent(conn,1, "Bob", "alice@gmail.com");
+            selectStudents(conn);
+            deleteStudent(conn, 1);
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void insertStudent(Connection conn, String name, String email){
+        String sql = "INSERT INTO student (name, email) " + "VALUES (' " + name + "','" + email  + "')";
+        try (Statement stmt = conn.createStatement()){
+            int rows = stmt.executeUpdate(sql);
+            System.out.println("INSERTED: " +rows);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void selectStudents(Connection conn){
+        String sql = "SELECT * FROM student";
+        try (Statement stmt = conn.createStatement()){
+            ResultSet resultSet = stmt.executeQuery(sql);
+            System.out.println("Student ListL");
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                System.out.println(id + " : " + name + " : " + email);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateStudent(Connection conn, int id, String name, String email){
+        String sql = "UPDATE student SET name = '" + name + "', email = '" + email + "' WHERE id = " + id;
+        // UPDATE student SET name = 'ALice', email = 'alice@gmail.com'
+        // WHERE id = 10
+        try (Statement stmt = conn.createStatement()){
+            int rows = stmt.executeUpdate(sql);
+            System.out.println("UPDATED: " +rows);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteStudent(Connection conn, int id){
+        String sql = "DELETE FROM student WHERE id = " +id;
+        try (Statement stmt = conn.createStatement()){
+            int rows = stmt.executeUpdate(sql);
+            System.out.println("DELETED: " +rows);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -22,5 +81,4 @@ public class JDBCDemo {
                 throw new RuntimeException(e);
             }
         }
-    }
-}
+ */
