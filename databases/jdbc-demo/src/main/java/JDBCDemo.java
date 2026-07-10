5eb7ebd1f74c127f04c6ae2c739051191f3a790c
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-10 23:05:51 +0530

    refactor: use PreparedStatement for JDBC queries

--- databases/jdbc-demo/src/main/java/JDBCDemo.java
+++ databases/jdbc-demo/src/main/java/JDBCDemo.java
@@ -9,7 +9,7 @@ public class JDBCDemo {
         try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);){
             System.out.println("Connected to the database");
 //            insertStudent(conn,"Alice", "alice@gmail.com");
-//            updateStudent(conn,1, "Bob", "alice@gmail.com");
+            updateStudent(conn,2, "Bob", "alice@gmail.com");
 //            selectStudents(conn);
 //            deleteStudent(conn, 1);
         }catch (SQLException e) {
@@ -18,10 +18,13 @@ public class JDBCDemo {
     }
 
     private static void insertStudent(Connection conn, String name, String email){
-        String sql = "INSERT INTO student (name, email) " + "VALUES (' " + name + "','" + email  + "')";
-        try (Statement stmt = conn.createStatement()){
-            int rows = stmt.executeUpdate(sql);
-            System.out.println("INSERTED: " +rows);
+//        String sql = "INSERT INTO student (name, email) " + "VALUES ('" + name + "','" + email  + "')";
+        String sql = "INSERT INTO student (name, email) VALUES (?, ?)";
+        try (PreparedStatement pStmt = conn.prepareStatement(sql)){
+            pStmt.setString(1, name);
+            pStmt.setString(2, email);
+            int rows = pStmt.executeUpdate();
+            System.out.println("INSERTED: " + rows);
         }catch (SQLException e){
             e.printStackTrace();
         }
@@ -31,7 +34,7 @@ public class JDBCDemo {
         String sql = "SELECT * FROM student";
         try (Statement stmt = conn.createStatement()){
             ResultSet resultSet = stmt.executeQuery(sql);
-            System.out.println("Student ListL");
+            System.out.println("Student List");
             while (resultSet.next()){
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
@@ -44,21 +47,27 @@ public class JDBCDemo {
     }
 
     private static void updateStudent(Connection conn, int id, String name, String email){
-        String sql = "UPDATE student SET name = '" + name + "', email = '" + email + "' WHERE id = " + id;
+//        String sql = "UPDATE student SET name = '" + name + "', email = '" + email + "' WHERE id = " + id;
+        String sql = "UPDATE student SET name = ?, email = ? WHERE id = ?";
         // UPDATE student SET name = 'ALice', email = 'alice@gmail.com'
         // WHERE id = 10
-        try (Statement stmt = conn.createStatement()){
-            int rows = stmt.executeUpdate(sql);
-            System.out.println("UPDATED: " +rows);
+        try (PreparedStatement pStmt = conn.prepareStatement(sql)){
+            pStmt.setString(1, name);
+            pStmt.setString(2, email);
+            pStmt.setInt(3, id);
+            int rows = pStmt.executeUpdate();
+            System.out.println("UPDATED: " + rows);
         }catch (SQLException e){
             e.printStackTrace();
         }
     }
 
     private static void deleteStudent(Connection conn, int id){
-        String sql = "DELETE FROM student WHERE id = " +id;
-        try (Statement stmt = conn.createStatement()){
-            int rows = stmt.executeUpdate(sql);
+//        String sql = "DELETE FROM student WHERE id = " +id;
+        String sql = "DELETE FROM student WHERE id = ?";
+        try (PreparedStatement pStmt = conn.prepareStatement(sql)){
+            pStmt.setInt(1, id);
+            int rows = pStmt.executeUpdate();
             System.out.println("DELETED: " +rows);
         }catch (SQLException e){
             e.printStackTrace();
